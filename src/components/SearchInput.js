7a60dbb43e9dd6b293c65ab0aa0a31fc7b7c96e4
Jeffery Jackson, Jr <jefferyjackson.jr@me.com>
2020-02-15 13:37:04 -0600

    linting

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -4,7 +4,7 @@ import "../styles/SearchInput.css"
 
 export default function SearchInput() {
 	return <div className="SearchInput">
-		<img src={require("../assets/search-icon.png")} alt="search-input-icon"/>
+		<img src={require(`../assets/search-icon.png`)} alt="search-input-icon"/>
 		<input type="text" placeholder="What ya looking for?" maxlength={50} autofocus />
 	</div>
 }
