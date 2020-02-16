d4116043f3f7f1ed23cc982c574450df883fe0f6
Jeffery Jackson, Jr <jefferyjackson.jr@me.com>
2020-02-15 18:33:58 -0600

    Added copy functionality to GIF component and adjusted styling

--- src/components/GIF.js
+++ src/components/GIF.js
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 
 import "../styles/GIF.css"
 
-export default function GIF({ url, mp4 }) {
+export default function GIF({ title, url, mp4 }) {
+	const urlRef = useRef(null)
+
+	const [copied, setCopied] = useState(false)
+
+	function copyToClipboard(e) {
+		urlRef.current.select()
+		document.execCommand(`copy`)
+
+		setCopied(true)
+
+		setTimeout(() => setCopied(false), 3000)
+	}
+
 	return <div className="GIF">
-		<video autoPlay loop>
+		<video autoPlay loop title={title}>
 			<source type="video/mp4" src={mp4}></source>
 		</video>
 		<div className="button-container">
-			<button>Copy URL</button>
+			<input type="text" ref={urlRef} value={url} readOnly />
+
+			{document.queryCommandSupported(`copy`) && <button onClick={copyToClipboard}>{copied ? `Copied!` : `Copy`}</button>}
 		</div>
 	</div>
 }
