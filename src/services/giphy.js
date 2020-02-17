71b52ebd9717dd9d519458fc99772628ec256b54
Jeffery Jackson, Jr <jefferyjackson.jr@me.com>
2020-02-17 04:22:29 -0600

    Updating JSDocs

--- src/services/giphy.js
+++ src/services/giphy.js
@@ -1,6 +1,15 @@
 const apiKey = `1HtuTdKSbKUcEyVW0UQxhntyEQoI56gy`
 
 /**
+ * An object representing a GIF
+ * @typedef {Object} GIF
+ * @property {String} id A unique identifier
+ * @property {String} url The url of the gif
+ * @property {String} mp4 The url to the mp4 version of the gif
+ * @property {String} title The title of the gif
+ */
+
+/**
  * A simple helper function for generating a GIPHY endpoint URL
  * @param {String} endpoint The specific endpoint to target within the GIPHY API
  * @returns {String} A string representing the full endpoint URL
@@ -21,9 +30,16 @@ async function fetchAsync(...args) {
 }
 
 /**
+ * @typedef {Object} SearchResponse
+ * @property {Array<GIF>} data The results of the search
+ * @property {Object} pagination Pagination data for the response
+ */
+/**
  * A function to perform a GIPHY API search
  * @param {String} searchTerm The term to search the GIPHY API for
- * @returns {Array} An array of GIFs
+ * @param {number} limit The number of results to return
+ * @param {number} offset The offset at which the results will begin
+ * @returns {SearchResponse} An object containing array of GIFs and pagination info
  */
 export async function search(searchTerm = ``, limit = 6, offset = 0) {
 	const { data, pagination } = await fetchAsync(`${endpointUrl(`search`)}&q=${(searchTerm).replace(/\s/g, `+`)}&limit=${limit}&offset=${offset}`)
@@ -36,7 +52,7 @@ export async function search(searchTerm = ``, limit = 6, offset = 0) {
 
 /**
  * A function to perform a GIPHY API random request
- * @returns {GIF} An object representing a GIF
+ * @returns {GIF} A GIF
  */
 export async function random() {
 	const { data: { id, title, url, images: { original: { mp4 } } } } = await fetchAsync(`${endpointUrl(`random`)}`)
